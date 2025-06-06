c2cea01a192a57a10a15d62a90166d99485dd137
onion_debugger <oshiole94@gmail.com>
2025-06-07 00:47:42 +0100

    Initial Commit

---
+++ StateApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StateApp
+//
+//  Created by onion_debugger on 07/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StateApp/StateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAppApp.swift
+//  StateApp
+//
+//  Created by onion_debugger on 07/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct StateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
